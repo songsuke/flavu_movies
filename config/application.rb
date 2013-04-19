2039f231d4a546a7f809dd28f85e8e638554ab5f
songsuke <songsukeja@gmail.com>
2013-04-19 15:35:37 -0700

    fix js

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,6 @@ module EasilyxMovie
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.assets.paths << Rails.root.join("app", "assets", "flash")
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -29,7 +28,7 @@ module EasilyxMovie
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-    config.assets.compile = true
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
@@ -55,7 +54,7 @@ module EasilyxMovie
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
