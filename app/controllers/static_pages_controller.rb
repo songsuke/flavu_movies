dda233279da10c65392ca06679c35e3ca0f1a5fc
songsuke <songsukeja@gmail.com>
2013-04-19 10:18:18 -0700

    init

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -93,6 +93,8 @@ require 'open-uri'
     else
         #@a1=session[:auth]
         puts session[:check_guest]
+        puts session[:auth]
+        puts session[:guest_auth]
         if session[:check_guest] == 'true'
           @url = "http://flavumovies.herokuapp.com/movies.json?latitude=#{session[:latitude]}&longitude=#{session[:longitude]}"
           @movies =HTTParty.get(@url, body: {user: {auth_token: session[:guest_auth]}}).parsed_response
