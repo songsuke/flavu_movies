37c425d950f4e234f3673ab1f7c7dc9664e8c803
songsuke <songsukeja@gmail.com>
2013-05-30 09:34:49 -0700

    init

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1152,10 +1152,12 @@ UDPSocket.open do |s|
 
   def reset_password
     puts params[:reset_password_token]
+    session[:reset_password_token] = params[:reset_password_token]
     #puts params[:reset_password_token]
     puts params[:confirm]
     if params[:confirm]=="confirm"
-      puts "yes"
+      puts params[:password]
+      puts params[:password_confirmation]       
       @url = "http://flavumovies.herokuapp.com/users/password.json"
       @reset_password =HTTParty.put(@url, body: { user: {reset_password_token: params[:reset_password_token], password: params[:password], password_confirmation: params[:password_confirmation] }}).parsed_response
       session[:auth]=@reset_password['auth_token']   
