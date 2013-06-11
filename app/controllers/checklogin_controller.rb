fb3a264e6ae4b1db65da4aabab92e946a0f564d9
songsuke <songsukeja@gmail.com>
2013-06-11 10:19:57 -0700

    init

--- app/controllers/checklogin_controller.rb
+++ app/controllers/checklogin_controller.rb
@@ -17,9 +17,12 @@ def checklogin
         cookies.permanent.signed[:username] = {value: "#{@users['username']}"}
         #cookies.signed[:username] ={secure: true, httponly: true, domain: 'flavu.com', value: "true"}        
         cookies.permanent.signed[:user] = {value: "#{@users}"}
-        cookies.permanent.signed[:auth] = {secure: true,value: "#{@users['auth_token']}"}
+        if params[:checkbox]=1
+          cookies.permanent.signed[:auth] = {secure: true,value: "#{@users['auth_token']}"}
+        else
+          cookies.signed[:auth] = {secure: true,value: "#{@users['auth_token']}"}
+        end
         #cookies.signed[:auth] ={secure: true, httponly: true, domain: 'flavu.com', value: "#{@auth_token}"}
-        puts cookies.signed[:auth]
         #cookies.signed[:latitude] = '49.28385281'
        # cookies.signed[:longitude] = '-123.1120815'
         #@username=cookies.signed[:username]
